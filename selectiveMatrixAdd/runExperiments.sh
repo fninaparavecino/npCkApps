5b238b664a854db8d6baa185b6fff02dc577a4be
Fanny Nina Paravecino <fninaparavecino@ece.neu.edu>
2016-10-12 18:00:52 -0400

    Reduce input size to avoid crashing in NP for selectiveMatrixAdd

--- selectiveMatrixAdd/runExperiments.sh
+++ selectiveMatrixAdd/runExperiments.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "Non NP Experiments ------------------------------"
-./selectiveMatrixAddNonNP --size 1024
-./selectiveMatrixAddNonNP --size 2048
-./selectiveMatrixAddNonNP --size 4096
-./selectiveMatrixAddNonNP --size 8192
-echo "End ------------------------------------------------------"
+echo "------------------ Non NP Experiments ------------------------------"
+./selectiveMatrixAddNonNP --size 1024 --gpu 3
+./selectiveMatrixAddNonNP --size 2048 --gpu 3
+./selectiveMatrixAddNonNP --size 4096 --gpu 3
+./selectiveMatrixAddNonNP --size 7168 --gpu 3
+echo "End ----------------------------------------------------------------"
 
-echo "Non NP Experiments ------------------------------"
-./selectiveMatrixAddNP --size 1024
-./selectiveMatrixAddNP --size 2048
-./selectiveMatrixAddNP --size 4096
-./selectiveMatrixAddNP --size 8192
-echo "End --------------------------------------------------"
+echo "----------------- NP Experiments -----------------------------------"
+./selectiveMatrixAddNP --size 1024 --gpu 3
+./selectiveMatrixAddNP --size 2048 --gpu 3
+./selectiveMatrixAddNP --size 4096 --gpu 3
+./selectiveMatrixAddNP --size 7168 --gpu 3
+echo "End ----------------------------------------------------------------"
