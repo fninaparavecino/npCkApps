d65662238d4055c54794bfb3044acf5f0add543d
Fanny Nina Paravecino <fninaparavecino@gmail.com>
2017-04-05 18:08:32 -0400

    Re-structuring accl App

--- accl/src/gatherMetrics.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-nvprof --metrics inst_executed,warp_execution_efficiency,cf_executed ./acclNonNP
-
-echo "NP METRICS------------------------------------------------------"
-#!/bin/bash
-nvprof --metrics inst_executed,warp_execution_efficiency,cf_executed ./acclNP
-
